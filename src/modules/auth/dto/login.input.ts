771f61a3592669018d08c9a677197b198402399b
Mohammad Shahi <mohammad.mft.shahi@gmail.com>
2023-08-11 21:12:44 +0330

    add validation to login and register

--- src/modules/auth/dto/login.input.ts
+++ src/modules/auth/dto/login.input.ts
@@ -1,13 +1,19 @@
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { IsPhoneNumber, IsNotEmpty, IsBoolean } from 'class-validator';
 
 @InputType()
 export class CheckCode {
+  @IsNotEmpty({ message: 'کد ارسال شده الزامی میباشد' })
   @Field(() => String)
   code: string;
 
+  @IsPhoneNumber('IR', { message: 'فرمت شماره مبایل صحیح نمیباشد' })
+  @IsNotEmpty({ message: 'شماره مبایل الزامی میباشد' })
   @Field((returns) => String)
   phoneNumber: string;
 
+  @IsNotEmpty({ message: 'من را بخاطر بسپار الزامی میباشد' })
+  @IsBoolean({ message: 'من را بخاطر بسپار باید بولین باشد' })
   @Field((returns) => Boolean)
   rememberMe: boolean;
 }
--- src/modules/auth/dto/register.input.ts
+++ src/modules/auth/dto/register.input.ts
@@ -1,13 +1,19 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
-
+import { IsString, IsEmail, IsNotEmpty, IsPhoneNumber } from 'class-validator';
 @InputType()
 export class RegisterInput {
+  @IsPhoneNumber('IR', { message: 'فرمت شماره مبایل وارد شده صحیح نمیباشد' })
+  @IsNotEmpty({ message: 'شماره مبایل الزامی میباشد' })
   @Field(() => String, { description: 'phone number' })
   phoneNumber: string;
 
+  @IsString({ message: 'نام و نام خانوادگی باید رشته باشد' })
+  @IsNotEmpty({ message: 'نام و نام خانوادگی الزامی میباشد' })
   @Field(() => String, { description: 'user fullname' })
   fullName: string;
 
+  @IsEmail()
+  @IsNotEmpty({ message: 'ایمیل الزامی میباشد' })
   @Field(() => String, { description: 'user email' })
   email: string;
 }
